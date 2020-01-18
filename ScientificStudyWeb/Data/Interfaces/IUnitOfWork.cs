0839610f329a22f81c5cc076af3efa0f93b7d2b3
teodora <teodora.petrovic.best@gmail.com>
2020-01-18 10:56:09 +0100

    Adding UnitOfWork

--- ScientificStudyWeb/Data/Interfaces/IUnitOfWork.cs
+++ ScientificStudyWeb/Data/Interfaces/IUnitOfWork.cs
@@ -3,9 +3,15 @@ using System.Threading.Tasks;
 
 namespace ScientificStudyWeb.Data.Interfaces
 {
-    public interface IUnitOfWork: IDisposable
+    public interface IUnitOfWork : IDisposable
     {
-        void SaveChanges();
-         Task SaveChangesAsync();
+        IExperimentRepository experimentRepository { get; }
+        IGroupRepository groupRepository { get; }
+        IStudyRepository studyRepository { get; }
+        ITaskRepository taskRepository { get; }
+        ITestSubjectRepository testSubjectRepository { get; }
+
+        int SaveChanges();
+        Task SaveChangesAsync();
     }
 }
---
+++ ScientificStudyWeb/Data/UnitOfWork.cs
@@ -0,0 +1,46 @@
+using System.Threading.Tasks;
+using ScientificStudyWeb.Data.Interfaces;
+
+namespace ScientificStudyWeb.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ScientificStudiesRecordDbContext _context;
+
+        public IExperimentRepository experimentRepository { get; }
+
+        public IGroupRepository groupRepository { get; }
+
+        public IStudyRepository studyRepository { get; }
+
+        public ITaskRepository taskRepository { get; }
+
+        public ITestSubjectRepository testSubjectRepository { get; }
+
+        public UnitOfWork(ScientificStudiesRecordDbContext context)
+        {
+            _context = context;
+
+            experimentRepository = new ExperimentRepository(_context);
+            groupRepository = new GroupRepository(_context);
+            studyRepository = new StudyRepository(_context);
+            taskRepository = new TaskRepository (_context);
+            testSubjectRepository = new TestSubjectRepository(_context);
+
+        }
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        public int SaveChanges()
+        {
+            return _context.SaveChanges();
+        }
+
+        public Task SaveChangesAsync()
+        {
+            return _context.SaveChangesAsync();
+        }
+    }
+}
